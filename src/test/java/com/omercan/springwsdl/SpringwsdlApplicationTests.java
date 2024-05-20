0067ca1ec6c253ea1c6644b3e0451742b837feae
Ömercan Aras <canaras56789@gmail.com>
2024-05-20 13:35:03 +0200

    initial commit

---
+++ src/test/java/com/omercan/springwsdl/SpringwsdlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omercan.springwsdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwsdlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
